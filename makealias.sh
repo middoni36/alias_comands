bdb420ce9e8aeb7816dfff737a17335c907af231
elmehdi <mehdiamraoui36@gmail.com>
2021-10-26 18:47:18 +0200

    1st script

---
+++ makealias.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Enter EXECUTABLE  pwd: "
+read exec_pwd
+echo "enter aliasname: "
+read aliasname
+echo "${exec_pwd},,${aliasname} "
+create_alias(){
+
+cd
+
+myline_numb=$(awk '/#elmehdi alias/{print NR ;exit}' .bashrc) #.bashrc
+myline_numb=$((myline_numb + 1))
+sed -i "${myline_numb}i alias ${aliasname}='bash ${exec_pwd}'" .bashrc
+
+}
+create_alias
